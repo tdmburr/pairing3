61127f7f3b6c8d136ae334eb0003285deb121160
Trey Marshall <marshall.trey92@yahoo.com>
2022-11-10 18:04:12 -0600

    Add for statement to print numbers under 60

--- friends.js
+++ friends.js
@@ -1,6 +1,15 @@
 var numbers = [13, 21, 37, 44, 66, 70, 89, 91]
-    for (var i = 0; i < numbers.length; i++) {
-        if (numbers[i] > 30) {
-            console.log(numbers[i])
-        }
-    }
+
+for (var i = 0; i < numbers.length; i++) {
+    if (numbers[i] > 30) {
+    console.log(numbers[i])    
+    } 
+}
+
+console.log("------------")
+
+for (var i = 0; i < numbers.length; i++) {
+    if (numbers[i] < 60) {
+    console.log(numbers[i])    
+    } 
+}
